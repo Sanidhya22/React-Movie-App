f95cca7eb8b0c7998f7202bca682cd4421bfac01
Saniddhya Nigam <saniddhya_nigam@epam.com>
2022-10-28 14:37:14 +0530

    Test Husky

--- src/Components/MovieList/Movielist.tsx
+++ src/Components/MovieList/Movielist.tsx
@@ -3,6 +3,7 @@ import { Component } from "react";
 import Movielistheader from "../Header/Movielistheader";
 import "./movieList.scss";
 const Movielist = () => {
+  console.lo(hcah);
   return (
     <div className="Movielist">
       <Movielistheader />
