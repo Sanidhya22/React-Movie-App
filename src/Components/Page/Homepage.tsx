4ff50bc6ec30bb8e03fbcf4234a4bdd88613e29a
Saniddhya Nigam <saniddhya_nigam@epam.com>
2022-10-28 14:31:47 +0530

    Test Husky

--- src/Components/Page/Homepage.tsx
+++ src/Components/Page/Homepage.tsx
@@ -2,14 +2,14 @@ import * as React from "react";
 import { Component } from "react";
 import Footer from "../Footer/Footer";
 import Movielist from "../MovieList/movieList";
-import Navcontaner from "../Navbar/navContaner";
+import NavContaner from "../Navbar/navContaner";
 
 import "./Homepage.css";
 
 const Homepage = () => {
   return (
     <div className="homepage">
-      <Navcontaner />
+      <NavContaner />
       <Movielist />
       <Footer />
     </div>
